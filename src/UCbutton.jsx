3f2fd19ab2b97d0f32218281c84db6e6e0e6e396
Hassan <raj.hassan1824@gmail.com>
2025-01-09 14:24:42 +0500

    Branch2

---
+++ src/UCbutton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UCbutton({onclick}) {
+  return (
+    <div className='bg-black tect-white w-32 h-10 ' onClick={onclick}>
+        Click
+    </div>
+  )
+}
+
+export default UCbutton
